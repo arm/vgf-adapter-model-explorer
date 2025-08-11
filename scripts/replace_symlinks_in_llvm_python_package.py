e75c57f753f05eebcc8f9e0a021e738c2475e6bd
Carey Williams <carey.williams@arm.com>
2025-08-11 16:48:05 +0100

    chore: add explicit copyright headers

--- scripts/replace_symlinks_in_llvm_python_package.py
+++ scripts/replace_symlinks_in_llvm_python_package.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License v2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+
 """
 Script to replace symbolic links to Python files with the actual files.
 This will recursively process a directory and its subdirectories.
